65a9e4be1447e49f66299c505a0e347ce300a5d8
Vo Ngoc Tan Dung <vongoctandung2002@gmail.com>
2023-06-05 10:10:12 +0700

    Modified DBHelper.java change password from "123456" to "12345" Renamed package from "com.yowo..." to "com.yowu..."

--- yoga-center-project/src/java/com/yowu/util/DBHelpler.java
+++ yoga-center-project/src/java/com/yowu/util/DBHelpler.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.yowo.util;
+package com.yowu.util;
 
 import java.io.Serializable;
 import java.sql.Connection;
