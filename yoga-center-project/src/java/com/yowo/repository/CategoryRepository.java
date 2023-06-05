9958f49eac39da6374e018fbf2a3f8cefb3119cd
thanhlong109 <thinghimminiworld@gmail.com>
2023-06-05 21:58:03 +0700

    add category repo and course repo

---
+++ yoga-center-project/src/java/com/yowo/repository/CategoryRepository.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.yowo.repository;
+
+import com.yowu.model.Category;
+import com.yowu.util.DBHelpler;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ACER
+ */
+public class CategoryRepository {
+    
+    public List<Category> getAll(){
+        String sql = "select * from tblCategory";
+        List<Category> list = new ArrayList<>();
+        
+        try(PreparedStatement stmt = DBHelpler.makeConnection().prepareStatement(sql)){
+            try(ResultSet rs = stmt.executeQuery()){
+                while(rs.next()){
+                    Category c = new Category();
+                    c.setId(rs.getInt("category_id"));
+                    c.setName(rs.getString("category_name"));
+                    c.setIsActive(rs.getBoolean("category_is_active"));
+                    list.add(c);
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return list;
+    }
+    
+    public Category detail(int id){
+        String sql = "select * from tblCategory where category_id=? ";
+        try(PreparedStatement stmt = DBHelpler.makeConnection().prepareStatement(sql)){
+            stmt.setInt(1, id);
+            try(ResultSet rs = stmt.executeQuery()){
+                while(rs.next()){
+                    Category c = new Category();
+                    c.setId(rs.getInt("category_id"));
+                    c.setName(rs.getString("category_name"));
+                    c.setIsActive(rs.getBoolean("category_is_active"));
+                    return c;
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+    public boolean update(Category c){
+        String sql = "update tblCategory set category_name=? , category_is_active=? where category_id=? ";
+        int status = 0;
+        try(PreparedStatement stmt = DBHelpler.makeConnection().prepareStatement(sql)){
+            stmt.setString(1, c.getName());
+            stmt.setBoolean(2, c.isIsActive());
+            stmt.setInt(3, c.getId());
+            status = stmt.executeUpdate();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return status==1;
+    }
+    public boolean delete(int id){
+         String sql = "update tblCategory set category_is_active=? where category_id=? ";
+        int status = 0;
+        try(PreparedStatement stmt = DBHelpler.makeConnection().prepareStatement(sql)){
+            stmt.setBoolean(1, false);
+            stmt.setInt(2, id);
+            status = stmt.executeUpdate();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return status==1;
+    }
+    
+    public static void main(String[] args) {
+        CategoryRepository cr = new CategoryRepository();
+        System.out.println(cr.getAll().get(0).getName());
+    }
+}
---
+++ yoga-center-project/src/java/com/yowo/repository/CourseRepository.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.yowo.repository;
+
+
+import com.yowu.model.Category;
+import com.yowu.model.Course;
+import com.yowu.util.DBHelpler;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ACER
+ */
+public class CourseRepository {
+     public List<Course> getAll(){
+        String sql = "select * from tblCourse";
+        List<Course> list = new ArrayList<>();
+        
+        try(PreparedStatement stmt = DBHelpler.makeConnection().prepareStatement(sql)){
+            try(ResultSet rs = stmt.executeQuery()){
+                while(rs.next()){
+                    CategoryRepository cr = new CategoryRepository();
+                    Course c = new Course();
+                    c.setId(rs.getInt("course_id"));
+                    c.setCategory(cr.detail(rs.getInt("category_id")));
+                    c.setDetail(rs.getString("course_detail"));
+                    c.setDuration(rs.getInt("course_duration"));
+                    c.setImg(rs.getString("course_img"));
+                    c.setIsActive(rs.getBoolean("course_is_active"));
+                    c.setPrice(rs.getFloat("course_price"));
+                    c.setTitle(rs.getString("course_title"));
+                    list.add(c);
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return list;
+    }
+    
+    public Course detail(int id){
+        String sql = "select * from tblCourse where course_id=? ";
+        try(PreparedStatement stmt = DBHelpler.makeConnection().prepareStatement(sql)){
+            stmt.setInt(1, id);
+            try(ResultSet rs = stmt.executeQuery()){
+                while(rs.next()){
+                    CategoryRepository cr = new CategoryRepository();
+                    Course c = new Course();
+                    c.setId(rs.getInt("course_id"));
+                    c.setCategory(cr.detail(rs.getInt("category_id")));
+                    c.setDetail(rs.getString("course_detail"));
+                    c.setDuration(rs.getInt("course_duration"));
+                    c.setImg(rs.getString("course_img"));
+                    c.setIsActive(rs.getBoolean("course_is_active"));
+                    c.setPrice(rs.getFloat("course_price"));
+                    c.setTitle(rs.getString("course_title"));
+                    return c;
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+    public boolean update(Course c){
+        String sql = "update tblCourse set course_title=? , course_is_active=? ,course_detail=? , course_duration=? , course_img=? , course_price , category_id=?  where course_id=? ";
+        int status = 0;
+        try(PreparedStatement stmt = DBHelpler.makeConnection().prepareStatement(sql)){
+            stmt.setString(1, c.getTitle());
+            stmt.setBoolean(2, c.isIsActive());
+            stmt.setString(3, c.getDetail());
+            stmt.setInt(4, c.getDuration());
+            stmt.setString(5, c.getImg());
+            stmt.setFloat(6, c.getPrice());
+            stmt.setInt(7,c.getCategory().getId());
+            stmt.setInt(8, c.getId());
+            status = stmt.executeUpdate();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return status==1;
+    }
+    public boolean delete(int id){
+         String sql = "update tblCourse set course_is_active=? where course_id=? ";
+        int status = 0;
+        try(PreparedStatement stmt = DBHelpler.makeConnection().prepareStatement(sql)){
+            stmt.setBoolean(1, false);
+            stmt.setInt(2, id);
+            status = stmt.executeUpdate();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return status==1;
+    }
+    
+    public static void main(String[] args) {
+        CourseRepository cr = new CourseRepository();
+        System.out.println(cr.detail(1).getTitle());
+    }
+}
