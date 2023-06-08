2e0fb9ae04d048cf135e34eebfea4217861118bd
An Hoang Le <anlehoang6789@gmail.com>
2023-06-08 22:04:06 +0700

    add instructorProfile.html and instructorProfile.css to layout

--- yoga-center-project/src/java/com/yowu/controller/client/ClientLoginController.java
+++ yoga-center-project/src/java/com/yowu/controller/client/ClientLoginController.java
@@ -7,27 +7,38 @@ package com.yowu.controller.client;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author ACER
+ * @author localboss
  */
-public class ClientHomeController extends HttpServlet {
+@WebServlet(name = "ClientLoginController", urlPatterns = {"/ClientLoginController"})
+public class ClientLoginController extends HttpServlet {
 
     private final String HOME_PAGE = "Home.jsp";
-    private final String LOGIN_CONTROLLER = "ClientLoginController";
-    
+    private final String LOGIN_PAGE = "login_register.jsp";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
         String url = HOME_PAGE;
-        
-        try {
+        try  {
             
-        } finally {
+        } finally{
+            
         }
     }
 
